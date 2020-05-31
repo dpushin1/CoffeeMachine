e1e17ffa82fd5fb5e2a09854c98b34a55085353d
Danil Pushin <danilkapushin1@gmail.com>
2020-05-31 15:51:58 +0300

    Added constant

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        final String TEST_MESSAGE = "Test";
+        System.out.println(TEST_MESSAGE);
     }
 }
